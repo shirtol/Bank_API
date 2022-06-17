32af1ffe08b88857d19c582f275e1606d22629a8
shirtol <shirzahavi7@gmail.com>
2022-06-17 15:59:27 +0300

    Added update credit function

--- server/routes/userController.route.js
+++ server/routes/userController.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { WITHDRAW_TYPE_CASH, WITHDRAW_TYPE_CREDIT } from "../consts.js";
+import { UPDATE_TYPE_CASH, UPDATE_TYPE_CREDIT } from "../consts.js";
 import {
     getUserData,
     getAllUsers,
     addNewUser,
     depositCash,
     withdrawMoney,
+    updateCredit,
 } from "../utils.js";
 
 export const route = express.Router();
@@ -54,18 +55,27 @@ route.put("/users/cash/deposit", (req, res) => {
 
 route.put("/users/cash/withdraw", (req, res) => {
     try {
-        withdrawMoney(req.body, WITHDRAW_TYPE_CASH);
+        withdrawMoney(req.body, UPDATE_TYPE_CASH);
         res.status(200).json(getUserData(req.body.userId));
     } catch (err) {
-        res.status(404).send(err.message);
+        res.status(400).send(err.message);
     }
 });
 
 route.put("/users/credit/withdraw", (req, res) => {
     try {
-        withdrawMoney(req.body, WITHDRAW_TYPE_CREDIT);
+        withdrawMoney(req.body, UPDATE_TYPE_CREDIT);
         res.status(200).json(getUserData(req.body.userId));
     } catch (err) {
-        res.status(404).send(err.message);
+        res.status(400).send(err.message);
+    }
+});
+
+route.put("/users/credit/update", (req, res) => {
+    try {
+        updateCredit(req.body);
+        res.status(200).json(getUserData(req.body.userId));
+    } catch (err) {
+        res.status(400).send(err.message);
     }
 });
