dcc571fbc312f9395fa8c6a53c1067572cfd2f99
shirtol <shirzahavi7@gmail.com>
2022-06-18 15:18:56 +0300

    Added react

--- server/app.js
+++ server/app.js
@@ -6,11 +6,17 @@ import { route as accountsRoute } from "./routes/accountController.route.js";
 import { amountValidation } from "./middlewares/amountValidationMiddlewares.js";
 import { userExistValidation } from "./middlewares/userExistValidationMiddlewares.js";
 import { accountExistValidation } from "./middlewares/accountExistValidationMiddleware.js";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use("/user", userExistValidation);
 app.use(["/user/cash", "/user/credit"], accountExistValidation);
 app.use(["/user/cash", "/user/credit"], amountValidation);
@@ -20,6 +26,10 @@ app.use((err, req, res, next) => {
 });
 app.use("/", usersRoute);
 app.use("/", accountsRoute);
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 app.listen(PORT, (req, res) => {
     console.log(`Listen to port ${PORT}`);
